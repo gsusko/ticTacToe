5dd525c351bddda4b19fdf711424776b7e3fb4ca
Greg Susko <gsusko1@gmail.com>
2017-09-25 15:25:38 -0700

    add winning combos

--- ticTacToe.js
+++ ticTacToe.js
@@ -3,5 +3,22 @@ var prompt = require('prompt');
 prompt.start();
 
 var board = [[1,2,3], [4,5,6], [7,8,9]];
-
 console.log(board);
+
+var winningCombos = [[1, 5, 9], [1, 4, 7], [1, 2, 3], [2, 5, 8], [3, 5, 7], [3, 6, 9], [4, 5, 6], [7, 8, 9]];
+prompt.get(['move'], (err, results) => {
+  if (err) {
+    throw err;
+  }
+  if (typeof JSON.parse(results.move) !== 'number') {
+    console.log('unable to make move');
+  } else {
+    console.log('added x to space ' + results.move);
+
+  }
+});
+
+function onErr(err) {
+   console.log(err);
+   return 1;
+ }
