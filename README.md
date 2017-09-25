3415a7fc4f738caf1a7c996dd0ad113ed2558411
Greg Susko <gsusko1@gmail.com>
2017-09-25 15:29:06 -0700

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+## Using this Repository
 npm install
 npm start to begin
--- ticTacToe.js
+++ ticTacToe.js
@@ -6,6 +6,7 @@ var board = [[1,2,3], [4,5,6], [7,8,9]];
 console.log(board);
 
 var winningCombos = [[1, 5, 9], [1, 4, 7], [1, 2, 3], [2, 5, 8], [3, 5, 7], [3, 6, 9], [4, 5, 6], [7, 8, 9]];
+
 prompt.get(['move'], (err, results) => {
   if (err) {
     throw err;
